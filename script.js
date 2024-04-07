b1bfc7731a8a62395863aae82df74dd3ecfda53d
faz0189 <128984518+faz0189@users.noreply.github.com>
2024-04-07 19:24:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+let para = document.querySelector("#timer")
+
+
+function currentTime(){
+
+    let date = new Date()
+    let hour = date.getHours()
+
+    let min = date.getMinutes()
+    
+    let sec = date.getSeconds()
+    let todayDate = date.getDate()
+
+    let day = date.getDay()
+
+    let month = date.getMonth()
+
+    let year = date.getFullYear()
+
+    let time = hour+":"+min+":"+sec
+    para.innerHtml = `${month}/${day}/${year}, ${time}`
+
+    return `${month}/${day}/${year}, ${time}`
+}
+
+let ans = currentTime()
+alert(ans)
+
+setInterval(currentTime,1000)//your JS code here. If required.
